7a4d6dc34e5ab14a74413e91fc4a7634e8082e6c
rolandonajera <erolando.najera@gmail.com>
2021-10-11 21:38:16 -0500

    api greeting

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log("hello!");
+require('dotenv').config();
+const express = require('express');
+
+const app = express();
+
+const port = process.env.PORT;
+
+app.get( '/api/greeting', ( req, res ) => {
+    res.status( 200 ).json( { message: "Hello there!" } );
+});
+
+app.get( '*', ( req, res ) => {
+    res.status( 404 ).json( { message: "Endpoint not defined." } );
+});
+
+app.listen( port, () => console.log(`App running in port: ${ port }`));
